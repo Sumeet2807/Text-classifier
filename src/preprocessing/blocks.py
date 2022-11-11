b7540de01b02348719eb7fe5c27c7e042f0b94ff
Sumeet <sumeet2807a@gmail.com>
2022-11-10 21:10:36 -0500

    modular pipeline complete

--- src/preprocessing/blocks.py
+++ src/preprocessing/blocks.py
@@ -5,39 +5,45 @@ from nltk.stem import WordNetLemmatizer
 import re
 
 class Text_reducer():
-    def __init__(self,core_words=[]):
+    def __init__(self,core_words=[],remove_garbage=True,remove_numbers=True):
         self.core_words = core_words
-    def __call__(self,s): 
-        red_text = ''   
+        self.remove_garbage = remove_garbage
+        self.remove_numbers = remove_numbers
+    def __call__(self,x):   
         if len(self.core_words):
             match_pattern = r'(\b' + re.escape(self.core_words[0])
             for i in range(1,len(self.core_words)):
                 match_pattern += (r'\b|\b' + re.escape(self.core_words[i]))
             match_pattern += r'\b)'
-            sents = re.split(';|\.', s)
+            sents = re.split(';|\.|\n', x)
+            x=''
             for sent in sents:
-                if re.findall(match_pattern, sent):            
-                    sent = re.sub('[^A-Za-z0-9]+', ' ', sent)
-                    sent = re.sub('\d*\.?\d+', ' num ', sent)
-                    red_text = '\n'.join([red_text, sent])
+                if re.findall(match_pattern, sent): 
+                    if self.remove_garbage:           
+                        sent = re.sub('[^A-Za-z0-9]+', ' ', sent) #remove garbage
+                    if self.remove_numbers:
+                        sent = re.sub('\d*\.?\d+', ' num ', sent) #remove numbers
+                    x = '\n'.join([x, sent])
         else:
-            red_text = re.sub('[^A-Za-z0-9]+', ' ', s)
-            red_text = re.sub('\d*\.?\d+', ' num ', s)
+            if self.remove_garbage:           
+                x = re.sub('[^A-Za-z0-9]+', ' ', x) #remove garbage
+            if self.remove_numbers:
+                x = re.sub('\d*\.?\d+', ' num ', x) #remove numbers
             
-        return red_text
+        return x
 
-class Text_preprocessor():
+class Remove_stop_words_lemmatize():
     def __init__(self,force_lower_case=False,reduce_text=None,remove_stop_words=True, lemmatize=True):
         self.reduce_text = reduce_text
         self.remove_stop_words = remove_stop_words
         self.lemmatize = lemmatize
         self.force_lower_case = force_lower_case
     def __call__(self,x):
-        x = str(x)
-        if self.force_lower_case:
-            x = x.lower()
-        if self.reduce_text:
-            x = self.reduce_text(x)
+        # x = str(x)
+        # if self.force_lower_case:
+        #     x = x.lower()
+        # if self.reduce_text:
+        #     x = self.reduce_text(x)
         if self.remove_stop_words:        
             stop_words = set(stopwords.words('english'))
         if self.lemmatize:
@@ -54,6 +60,12 @@ class Text_preprocessor():
                 filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
 
 
-            x = ' '.join(filtered_sentence).lower()
+            x = ' '.join(filtered_sentence)
 
         return x
+
+
+def convert_lowercase(x):
+    return x.lower()
+
+
---
+++ src/preprocessing/preprocessor.py
@@ -0,0 +1,33 @@
+import preprocessing.blocks as blocks
+
+
+class Text_preprocessor_from_dict():
+    def __init__(self,preprocess_dict):
+
+        core_words = preprocess_dict['core-words']
+        remove_garbage = preprocess_dict['remove-garbage']
+        remove_numbers = preprocess_dict['remove-numbers']
+        remove_stopwords = preprocess_dict['remove-stopwords']
+        lemmatize = preprocess_dict['lemmatize']
+        force_lower_case = preprocess_dict['force-lower-case']
+
+        pipeline = []
+
+        if force_lower_case:
+            pipeline.append(blocks.convert_lowercase)
+
+        if core_words is None:
+            core_words = []
+
+        pipeline.append(blocks.Text_reducer(core_words,remove_garbage,remove_numbers))
+        pipeline.append(blocks.Remove_stop_words_lemmatize(remove_stopwords, lemmatize))
+
+        self.pipeline = pipeline
+
+    def __call__(self,x):
+        for block in self.pipeline:
+            x = block(x)
+
+        return x
+
+    
